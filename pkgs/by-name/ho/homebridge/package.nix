126f8de344068302337ebe73a80c9773aee1857e
Éric Peiffer <eric.peiffer@sinfony.eu>
2025-03-10 17:36:47 +0100

    homebridge: add darwin support

--- pkgs/by-name/ho/homebridge/package.nix
+++ pkgs/by-name/ho/homebridge/package.nix
@@ -34,7 +34,7 @@ buildNpmPackage rec {
     homepage = "https://github.com/homebridge/homebridge";
     license = lib.licenses.asl20;
     mainProgram = "homebridge";
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
     maintainers = with lib.maintainers; [ fmoda3 ];
   };
 }
